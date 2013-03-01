e1c016b70cc98a6bea3e7de385be33f9b955920a
Norbert Crombach <norbert.crombach@primetheory.org>
2013-03-01 15:43:03 +0100

    Change instrument and listen method signatures

--- lib/vivaldi/configuration.rb
+++ lib/vivaldi/configuration.rb
@@ -7,18 +7,35 @@ module Vivaldi
       @listeners = []
     end
 
-    def instrument(name, *args, &block)
-      instrument = Instrument.play(name, *args, &block)
-      if instrument.nil?
-        raise ArgumentError, "unknown instrument: #{name.inspect}"
+    def instrument(instrument_or_name, *args, &block)
+      instrument = if instrument_or_name.respond_to?(:play)
+        if args.empty?
+          instrument_or_name
+        else
+          raise ArgumentError, "invalid call signature"
+        end
+      elsif
+        Instrument.play(instrument_or_name, *args, &block)
+      else
+        raise ArgumentError, "unknown instrument: #{instrument_or_name.inspect}"
       end
 
       @instruments << instrument
       instrument
     end
 
-    def listen(klass, *args)
-      listener = klass.new(*args)
+    def listen(listener_or_klass, *args)
+      listener = if listener_or_klass.is_a?(Class)
+        listener_or_klass.new(*args)
+      elsif listener_or_klass.respond_to?(:observe)
+        if args.empty?
+          listener_or_klass
+        else
+          raise ArgumentError, "invalid call signature"
+        end
+      else
+        raise ArgumentError, "invalid listener"
+      end
 
       @listeners << listener
       listener
