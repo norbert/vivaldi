102b39246c7896b559185db43ada3209f3aedd78
Norbert Crombach <norbert.crombach@primetheory.org>
2013-03-01 19:32:54 +0100

    Fix note attributes

--- lib/vivaldi/note.rb
+++ lib/vivaldi/note.rb
@@ -1,22 +1,20 @@
 module Vivaldi
-  class Note < Struct.new(:type, :key, :value, :option)
+  class Note < Struct.new(:type, :key, :value, :rate)
     TYPES = [:count, :gauge, :timing, :set]
 
-    def initialize(type, key, value, option = nil)
+    attr_reader :arguments
+
+    def initialize(type, key, value, rate = nil)
       if TYPES.include?(type)
         self.type = type
       else
         raise ArgumentError, "invalid type: #{type.inspect}"
       end
-      self.key = key
-      self.value = value
-      self.option = option
-    end
-
-    def arguments
-      return @arguments if @arguments
-      @arguments = [key, value]
-      option.nil? ? @arguments : @arguments.push(option)
+      self.key = key.to_s.freeze
+      self.value = value.is_a?(Numeric) ? value : Float(value)
+      self.rate = rate.nil? ? 1.0 : Float(rate)
+      @arguments = [self.key, self.value]
+      @arguments.push(self.rate)
     end
   end
 end
